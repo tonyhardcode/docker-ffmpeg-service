98c14342d6a8b837056c9eb0d79d1f931505b5d4
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2018-11-15 15:02:41 +0100

    Update endpoints.js

--- app/endpoints.js
+++ app/endpoints.js
@@ -1,14 +1,8 @@
 exports.types = {
-    jpg: {
-        extension: 'jpg',
+    bmp: {
+        extension: 'bmp',
         outputOptions: [
-            '-pix_fmt yuv422p',
-        ],
-    },
-    mp3: {
-        extension: 'mp3',
-        outputOptions: [
-            '-codec:a libmp3lame',
+            '-s 128x32',
         ],
     },
     wav: {
@@ -18,23 +12,6 @@ exports.types = {
             '-ar 16000',
             '-t 20',
             '-sample_fmt s16'
-        ],
-    },
-    mp4: {
-        extension: 'mp4',
-        outputOptions: [
-            '-codec:v libx264',
-            '-profile:v high',
-            '-r 15',
-            '-crf 23',
-            '-preset ultrafast',
-            '-b:v 500k',
-            '-maxrate 500k',
-            '-bufsize 1000k',
-            '-vf scale=-2:640',
-            '-threads 8',
-            '-codec:a libfdk_aac',
-            '-b:a 128k',
         ],
     },
 };
