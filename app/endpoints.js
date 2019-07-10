85209043c9ff5dfc790e55339f49b42b0747bee4
tonyhardcode <43479991+tonyhardcode@users.noreply.github.com>
2019-07-10 13:48:37 -0700

    Update endpoints.js

--- app/endpoints.js
+++ app/endpoints.js
@@ -1,18 +1,37 @@
 exports.types = {
-    bmp: {
-        extension: 'bmp',
+    jpg: {
+        extension: 'jpg',
         outputOptions: [
-            '-s 128x32',
-            '-pix_fmt monow',
+            '-pix_fmt yuv422p',
         ],
     },
-    wav: {
-        extension: 'wav',
+    m4a: {
+        extension: 'm4a',
         outputOptions: [
-            '-ac 1',
-            '-ar 16000',
-            '-t 20',
-            '-sample_fmt s16'
+            '-codec:a libfdk_aac',
+        ],
+    },
+    mp3: {
+        extension: 'mp3',
+        outputOptions: [
+            '-codec:a libmp3lame',
+        ],
+    },
+    mp4: {
+        extension: 'mp4',
+        outputOptions: [
+            '-codec:v libx264',
+            '-profile:v high',
+            '-r 15',
+            '-crf 23',
+            '-preset ultrafast',
+            '-b:v 500k',
+            '-maxrate 500k',
+            '-bufsize 1000k',
+            '-vf scale=-2:640',
+            '-threads 8',
+            '-codec:a libfdk_aac',
+            '-b:a 128k',
         ],
     },
 };
